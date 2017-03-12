7225eac82aa0ad54cec5fb7fbd760ca402e3e52a
Antoine Langlet <langlet.antoine@gmail.com>
2017-03-12 22:55:54 +0100

    -a

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from apps.user import app_user
 from apps.auth import app_auth
 from apps.gif import app_gif
 from apps.transaction import app_transaction
-import os from pml
-import app
 
 app = Flask(__name__)
 app.register_blueprint(app_home)
@@ -15,6 +13,5 @@ app.register_blueprint(app_gif)
 app.register_blueprint(app_transaction)
 
 
-port = int(os.environ.get('PORT', 5000))
-
-app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run()
