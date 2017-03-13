2cb647f58dd54de89f991a75ab617bd1a14b03c1
Florian Mopin <florianmopin@MacBook-Pro-de-Florian.local>
2017-03-13 21:35:42 +0100

    clean branch

--- apps/home.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, Blueprint, jsonify, request, render_template
-from models import User, Token, Gif
-
-app_home = Blueprint('app_home', __name__)
-
-@app_home.route('/', methods=['GET'])
-def home():
-    return render_template('index.html', page='home'), 201
