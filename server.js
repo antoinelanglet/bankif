b1e88692adcfcb5efbccd704be4fec7ae0056c76
Antoine Langlet <langlet.antoine@gmail.com>
2017-03-14 12:43:07 +0100

    Installed Good packages

--- server.js
+++ server.js
@@ -1,28 +1,54 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Good = require('good');
 
-// Create a server with a host and port
 const server = new Hapi.Server();
-server.connection({
-	host: 'localhost',
-	port: 8000
+server.connection({ port: 3000, host: 'localhost' });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
 });
 
-// Add the route
-server.route ({
-	method: 'GET',
-	path: '/hello',
-	handler: function (request, reply) {
-		return reply('Hello world');
-	}
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
 });
 
-// Start the server
-server.start((err) => {
-	if (err) {
-		throw err;
-	}
+server.register({
+    register: Good,
+    options: {
+        reporters: {
+            console: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{
+                    response: '*',
+                    log: '*'
+                }]
+            }, {
+                module: 'good-console'
+            }, 'stdout']
+        }
+    }
+}, (err) => {
 
-	console.log(`Server running at: ${server.info.uri}`);
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start((err) => {
+
+        if (err) {
+            throw err;
+        }
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
 });
